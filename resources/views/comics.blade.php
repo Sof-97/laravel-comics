8ec8fdeea68b1deb288f4543b21540c7e85c0851
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-07-24 12:18:47 +0200

    Revert "Adv image not working, all data printed"
    This reverts commit 0d92dc54a15ee2f705dc134014695c23a5d13c00.

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -11,7 +11,7 @@
             <h2>{{$comic['title']}}</h2>
             <div class="info">
                 <div>
-                    <p>Us.Price:
+                    <p>Us.Price: 
                         <span>{{$comic['price']}}</span>
                     </p>
                     <p>AVAILABLE</p>
@@ -24,47 +24,7 @@
     </div>
 </div>
 <div class="container_grey">
-    <div class="row">
-        <div class="talent">
-            <h3>Talent</h3>
-            <div>
-                <p>Art by:</p>
-                <p>Written by:</p>
-            </div>
-            <div>
-                <p>
-                    @php
-                        $gigi = 'gigi'
-                    @endphp
-                    @foreach($comic['artists'] as $value)
-                        {{$value}} @if(count($comic['artists'])>1)
-                            -
-                        @endif
-                    @endforeach
-                </p>
-                <p>
-                @foreach($comic['writers'] as $value)
-                        {{$value}} @if(count($comic['writers'])>1)
-                            -
-                        @endif
-                    @endforeach
-                </p>
-            </div>
-        </div>
-        <div class="specs">
-            <h3>Specs</h3>
-            <div>
-                <p>Series:</p>
-                <p>U.S Price:</p>
-                <p>On Sale Date:</p>
-            </div>
-            <div>
-                <p>{{$comic['series']}}</p>
-                <p>{{$comic['price']}}</p>
-                <p>{{$comic['sale_date']}}</p>
-            </div>
-        </div>
-    </div>
-</div>11
+    <div class="row"></div>
+</div>
 
 @endsection
